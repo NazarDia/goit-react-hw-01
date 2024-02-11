7f69f13727de48109ca204b0194e9b65e524bacf
Nazar Dia <nazar.dia@gmail.com>
2024-02-11 17:51:14 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Profile } from './components/Profile/Profile';
 import { FriendList } from './components/FriendList/FriendList';
 import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
-import userData from './Data/userData.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
+import userData from './data/userData.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import './App.css';
 import '../node_modules/modern-normalize/modern-normalize.css';
 
